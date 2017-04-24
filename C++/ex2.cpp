1108ff10982a0a4f5ba1a0719dbabd069b558ea7
IBunDem <keni-less@mail.ru>
2017-04-24 22:28:23 +0600

    added very old examples

---
+++ C++/ex2.cpp
@@ -0,0 +1,41 @@
+﻿//задание:
+//Определить время окончания рабочего дня (в часах и минутах),
+//если известны время его начала (в часах и минутах) и продолжительность (вместе с обедом) (в часах и минутах).
+
+//Краткое описание:
+//Получаешь время начала, потом получаешь время окончания через ввод. Потом получаешь через сложение часов и минут
+//конечное время. Учитывая, что в часе 60 минут.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+ setlocale(LC_CTYPE, "rus"); // отображение кириллицы в консоли
+
+ float starthour, endhour, workhours, startminute, endminute, workminutes;
+
+ cout << "Введите час начала работы: ";
+ cin >> starthour;
+ cout << "Введите минуту начала работы: ";
+ cin >> startminute;
+ cout << "Введите количество часов работы: ";
+ cin >> workhours;
+ cout << "Введите количество минут работы: ";
+ cin >> workminutes;
+
+ endhour = starthour + workhours;
+ endminute = startminute + workminutes;
+
+ while(endminute > 60) {
+ if(endminute > 60) {
+ endminute -= 60;
+ endhour++;
+ }
+ else break;
+ }
+ if(endhour>23) {
+ endhour -= 23;
+ }
+ cout<<"Время окончания работы: " << endhour << " часов, " << endminute << " минут. " <<endl;
+ return 0;
+}
---
+++ C++/ex3.cpp
@@ -0,0 +1,29 @@
+﻿//Задание:
+//Вывести количество нулей в конце факториала числа введенного пользователемъ
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  setlocale(0,"");
+  int n=1, factorial=1, count =0;
+
+  cout<<"Введите число: "<<endl;
+  cin>>n;
+
+  for(int i=1; i<=n; i++){
+    factorial*=i;
+  }
+  cout<<factorial<<endl;
+
+  if(factorial%10 == 0){
+    while(factorial%10 ==0){
+      count++;
+      factorial/=10;
+    }
+  }
+  else cout<<"В конце нет нолей!" <<endl;
+  cout<<count<<endl;
+
+  return 0;
+}
